b09b2899bdd7c3461e587c546166c7e8ce5e426b
Doug Bergh <dougbergh@gmail.com>
2016-04-13 13:31:05 -0700

    timecard with google sheets

---
+++ timecard_ios/timecard/timecard-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//#import "GTL.framework/Headers/GTLService.h"
+//#import "GTL.framework/Headers/GTMOAuth2ViewControllerTouch.h"
+#import "GTL/GTMOAuth2ViewControllerTouch.h"
+#import "GTLDrive.h"
