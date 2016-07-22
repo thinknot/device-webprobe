a72e29d79c4ba794b5df454630ec585c06cf6089
Doug Bergh <dougbergh@gmail.com>
2016-07-22 09:44:19 -0700

    removed erroneously-added timecard_ios directory

--- timecard_ios/timecard/timecard-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-//#import "GTL.framework/Headers/GTLService.h"
-//#import "GTL.framework/Headers/GTMOAuth2ViewControllerTouch.h"
-#import "GTMOAuth2ViewControllerTouch.h"
-#import "GTLDrive.h"
